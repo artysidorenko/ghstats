f603594dce595512e640a30c3faa717ac5dfff92
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-03-31 22:28:34 +0100

    hard coded ID - do not use

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,12 +1,11 @@
 const AUTH_CONFIG = {
   domain: 'dev-8-2xd3l4.eu.auth0.com',
   clientId: 'xvOAhkUjsDBqDfFcPTaiwyf2Cn8gCQ7D',
-  callbackUrl: 'https://ghstats-arty.herokuapp.com/'
 }
 
-// if (process.env.REACT_APP_CONTEXT === 'localmachine') {
-//   AUTH_CONFIG.callbackUrl = 'http://localhost:3000'
-// } else AUTH_CONFIG.callbackUrl = 'http://localhost:3000'
+if (process.env.REACT_APP_CONTEXT === 'localmachine') {
+  AUTH_CONFIG.callbackUrl = 'http://localhost:3000'
+} else AUTH_CONFIG.callbackUrl = 'https://ghstats-arty.herokuapp.com/'
 
 export default AUTH_CONFIG
 
--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,32 @@ import '../styles/App.css'
 import MonthlyNewRepos from './queries/MonthlyNewRepos'
 import Languages from './queries/Languages';
 
+const queryAuth0API = (token, callback) => {
+  const request = require("request");
+
+  const options = {
+    method: 'GET',
+    url: `https://dev-8-2xd3l4.eu.auth0.com/api/v2/users`,
+    headers: { authorization: `Bearer ${token}` }
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error)
+    const responseArray = JSON.parse(body)
+    const githubToken = responseArray.find(e1 => e1.identities.some(e2 => e2.provider === 'github'))
+                                       .identities
+                                       .find(e2 => e2.provider === 'github')
+                                       .access_token
+    callback(githubToken)
+  });
+
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      token: null
+      token: null,
+      githubAccess: null
     }
   }
 
@@ -26,6 +47,11 @@ class App extends Component {
     if (localStorage.getItem('isLoggedIn') === 'true') {
       renewSession(authResult => {
         this.setState({ token: authResult.accessToken })
+        this.getAuth0APIAccess(githubToken => {
+          this.setState({ githubAccess: githubToken })
+          this.props.setToken(githubToken)
+          console.log(authResult.idToken)
+        })
       });
     }
     else if (this.props.history.location.hash) this.handleAuthentication(this.props.history)
@@ -35,16 +61,37 @@ class App extends Component {
     if (/access_token|id_token|error/.test(location.hash)) {
       this.props.auth.handleAuthentication(authResult => {
         this.setState({ token: authResult.accessToken })
+        console.log(authResult.idToken)
+        this.getAuth0APIAccess(githubToken => {
+          this.setState({ githubAccess: githubToken })
+          this.props.setToken(githubToken)
+        })
       })
     }
   }
 
+  getAuth0APIAccess = (callback) => {
+    const request = require("request");
+    const options = {
+      method: 'POST',
+      url: 'https://dev-8-2xd3l4.eu.auth0.com/oauth/token',
+      headers: { 'content-type': 'application/json' },
+      body: '{"client_id":"qdYnARIkxXPhghdw0EzOUP7CSnFMKj30","client_secret":"5zhOaRolUtd9I31gZhVNjYtLBtF-ijWOI4U9L0akgeKnVScimLhK6qPV3lkSawKG","audience":"https://dev-8-2xd3l4.eu.auth0.com/api/v2/","grant_type":"client_credentials"}'
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      const token = JSON.parse(body).access_token
+      queryAuth0API(token, githubToken => {
+        callback(githubToken)
+      })
+    });
+  }
+
   render() {
-    const { isAuthenticated, getAccessToken, getIdToken } = this.props.auth
-
+    const { isAuthenticated } = this.props.auth
+    const { githubAccess } = this.state
     const authCheck = isAuthenticated()
-
-    console.log('token: ' + this.state.token)
 
     return (
       <div className="App__container">
@@ -60,8 +107,8 @@ class App extends Component {
         )}
         {authCheck && <h2>Logged in!</h2>}
         <h3>Instructions once authorised: for each visualisation, select the desired parameters and click submit to fetch and visualise data</h3>
-        <MonthlyNewRepos />
-        <Languages />
+        <MonthlyNewRepos token={githubAccess} />
+        <Languages token={githubAccess} />
       </div>
     )
   }
